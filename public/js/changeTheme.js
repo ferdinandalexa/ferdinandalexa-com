6de6887036db613f0499e36531c46b2394bb6d62
Fernando Alejandro Yerena Ramos <ferdinandalexandros@gmail.com>
2022-06-16 11:11:03 -0500

    :sparkles: feat(theme): set system color scheme initially

--- public/js/changeTheme.js
+++ public/js/changeTheme.js
@@ -1,6 +1,9 @@
 const toggleTheme = document.getElementById('toggle-theme');
 let darkMode = localStorage.getItem('darkMode');
 
+const psc = window.matchMedia('(prefers-color-scheme: dark)');
+if(!darkMode) { darkMode = psc.matches ? 'enabled' : 'disabled' };
+
 if(darkMode === 'enabled') {
   document.body.classList.add('dark');
   toggleTheme.checked = true;
@@ -10,7 +13,7 @@ if(darkMode === 'enabled') {
 toggleTheme.addEventListener('change', ({target})=>{
   if(target.checked){
     darkMode = 'enabled'
-    document.body.classList.add('dark')
+    document.body.classList.toggle('dark')
   }
   else{
     darkMode = 'disabled'
