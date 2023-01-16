6be1490fc5d7afd15cd397abe531fc174a7cdfff
Fernando Alejandro Yerena Ramos <ferdinandalexandros@gmail.com>
2023-01-16 15:26:41 -0600

    ♻️ refactor: add time ago as a web component

--- src/utils/getTimeAgo.js
+++ src/utils/getTimeAgo.js
@@ -10,18 +10,18 @@ const DATE_UNITS = {
 
 const getSecondsDiff = timestamp => (Date.now() - timestamp) / 1000;
 
-function getUnitAndValueDate(seconsElapsed){
-  for (const [unit, secondsUnit] of Object.entries(DATE_UNITS)){
-    if(seconsElapsed >= secondsUnit || unit === 'second'){
+function getUnitAndValueDate(seconsElapsed) {
+  for (const [unit, secondsUnit] of Object.entries(DATE_UNITS)) {
+    if (seconsElapsed >= secondsUnit || unit === 'second') {
       const value = Math.floor(seconsElapsed / secondsUnit) * -1;
-      return {value, unit};
+      return { value, unit };
     }
   }
 }
 
-export default function getTimeAgo(timestamp, locale='es'){
+export default function getTimeAgo(timestamp, locale = 'es') {
   const rtf = new Intl.RelativeTimeFormat(locale);
   const seconsElapsed = getSecondsDiff(timestamp);
-  const {value, unit} = getUnitAndValueDate(seconsElapsed); 
+  const { value, unit } = getUnitAndValueDate(seconsElapsed);
   return rtf.format(value, unit);
 }
