560eaefa09dda7754c7bddc27fe727ece72aa5bb
Fernando Alejandro Yerena Ramos <ferdinandalexandros@gmail.com>
2023-06-06 20:33:40 -0600

    :recycle: refactor: change output mode

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,6 @@ import vercel from "@astrojs/vercel/serverless";
 export default defineConfig({
   site: 'https://ferdinandalexa.com',
   integrations: [mdx()],
-  output: "server",
+  output: "hybrid",
   adapter: vercel()
 });
